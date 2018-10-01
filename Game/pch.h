857def2d8d99ee270aef1a8c6431a49d5b23d31c
Liudas Augustinas <amxxl@inbox.lt>
2018-10-01 22:49:54 +0300

    Coding style correction.

--- Game/pch.h
+++ Game/pch.h
@@ -83,7 +83,7 @@ namespace DX
     public:
         com_exception(HRESULT hr) : result(hr) {}
 
-        virtual const char* what() const override
+        virtual char const* what() const override
         {
             static char s_str[64] = {};
             sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<unsigned int>(result));
